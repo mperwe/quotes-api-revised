0bc5357927a498a2d4392f63e322d6d9e3c4f426
mperwe <mperwe@gmail.com>
2024-08-09 12:36:42 +0200

    implement authorsRoutes

---
+++ routes/authorsRoutes.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const router = express.Router();
+const { getAuthors, getAuthorById, createAuthor, updateAuthor, deleteAuthor } = require('../controllers/authorsController');
+
+router.get('/', getAuthors);
+router.get('/:id', getAuthorById);
+router.post('/', createAuthor);
+router.put('/:id', updateAuthor);
+router.delete('/:id', deleteAuthor);
+
+module.exports = router;
