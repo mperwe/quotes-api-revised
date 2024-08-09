d855ff44dac9583419aa0a3747bdda5af207bc2a
mperwe <mperwe@gmail.com>
2024-08-09 13:41:47 +0200

    edit

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ app.use(logger);
 app.use('/authors', authorsRoutes);
 app.use('/quotes', quotesRoutes);
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
