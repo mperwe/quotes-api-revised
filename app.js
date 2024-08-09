3a190023e0f43502bb89babbffe0132da9a27e6e
mperwe <mperwe@gmail.com>
2024-08-09 12:42:46 +0200

    set Main Application

---
+++ app.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const morgan = require('morgan');
+const logger = require('./middleware/logger');
+
+const authorsRoutes = require('./routes/authorsRoutes');
+const quotesRoutes = require('./routes/quotesRoutes');
+
+const app = express();
+
+app.use(express.json());
+app.use(morgan('dev'));
+app.use(logger);
+
+app.use('/authors', authorsRoutes);
+app.use('/quotes', quotesRoutes);
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
